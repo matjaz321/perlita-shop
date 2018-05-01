1ebca1969ada5f29eca8e51111b623c3138d3b9d
Matjaz <matjaz.zavski15@gmail.com>
2018-05-01 18:31:37 +0200

    Added mailchimp, new block for subscribe

---
+++ web/modules/custom/ps_general/src/Plugin/Block/SubscribeListBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\ps_general\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+
+/**
+ * Provides a 'SubscribeListBlock' block.
+ *
+ * @Block(
+ *  id = "subscribe_list_block",
+ *  admin_label = @Translation("Subscribe list block"),
+ * )
+ */
+class SubscribeListBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManager definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new CopyrightBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManager $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [
+      '#theme' => 'ps_subscribe_list_block',
+    ];
+
+    return $build;
+  }
+
+}
